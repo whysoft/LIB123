acf8a3be00f1d79b35456125549b47e2417a7396
whysoft1 <whysoft@126.com>
2019-12-23 17:40:05 +0800

    V2019-12-23

---
+++ exercises/ex17_jsonwebtest/jsonwebtest.cpp
@@ -0,0 +1,159 @@
+//
+#include "2.h"
+#include "CJsonObject.h"
+
+
+
+// 
+bool IsOsWin()
+{
+#ifdef WINENV_
+	return true;
+#else
+	return false;
+#endif
+}
+
+
+ 
+std::string  v_strContent;
+
+
+void test_json()
+{
+    std::string strValue;
+    std::string s1;
+    neb::CJsonObject oJson;
+
+	strValue = " {"
+				"\"timeout\":12.5,"
+				"\"t2\":\"5\","
+				"\"t3\":5,"
+				"\"tt\":[]"
+			   "}";
+
+	oJson.Parse( strValue );
+	s1 = oJson.ToString();
+	std::cout << s1 << std::endl;
+
+	double f;
+	oJson.Get( "timeout", f );
+	std::cout << f << std::endl;
+
+	std::string s;
+	oJson.Get( "t3", s );
+	std::cout << s << std::endl;
+
+    neb::CJsonObject oj2;
+
+	oj2.Add( 234 );
+	oj2.Add( 234 );
+	oj2.Add( 234 );
+
+	oJson.Add( "tt4", oj2 );
+	s1 = oJson.ToString();
+	std::cout << s1 << std::endl;
+
+	v_strContent = s1;
+}
+
+
+
+extern funcandy_t *gp_fc1;
+funcandy_t *gp_fc1;
+
+
+
+//
+void test_web_recv_post()
+{
+	tbool rc;
+
+	gp_fc1 = new funcandy_t;
+	rc = gp_fc1->InitFlow( 8009 ); 
+	printf( "gp_fc1 init, rc=%d,Port=%d\n", (int)rc, (int)8009 );
+
+	//upload
+	class _test_upload : public funcandy_t::item_t	  
+	{
+	public:
+		_test_upload()
+		{
+			LinkFc( gp_fc1 );
+		}
+
+		virtual tbool lingy( const std::string &strFn , WNava &para , const std::string &strWholePara , funcandy_t::actwebele_t *pweb )
+		{
+			if( pweb->m_strCmdVerb != "POST" ) return 0;
+
+			neb::CJsonObject oJson;
+			std::string strRecv;
+			std::string s1;
+
+			pweb->ckPostBody.mk_str( strRecv );
+			oJson.Parse( strRecv );
+
+			s1 = oJson.ToString();
+			std::cout << s1 << std::endl;
+			
+			pweb->RtnWebContent( "", s1 );  
+
+			return 1;
+
+			// return 0;
+		}
+
+	};
+	_test_upload *p_test_upload = new _test_upload;
+}
+
+
+
+//
+void test_web_send_post()
+{
+	std::string strAddr = "127.0.0.1:8009";  
+	std::string strUniFn = "aaa"; 
+	std::string strMethod = "POST";
+	std::string strTxtType = "";
+	std::string strContent = v_strContent;
+	SCake		*pckBody = NULL ;
+	std::string strRtn1;
+	std::string *pstrRtn1 = &strRtn1;
+	SCake		ckRtn2 ;
+	SCake		*pckRtn2 = &ckRtn2 ;
+	std::string strCommuCR = "\r\n";
+
+	funcandy_t::WGet(   strAddr,
+						strUniFn,
+						strMethod,
+						strTxtType,
+						strContent,
+						pckBody,
+						pstrRtn1, 
+						pckRtn2,
+						strCommuCR );
+
+	printf( "HEADER:\n" );
+	printf( "%s:\n" , pstrRtn1->c_str() );
+
+
+	printf( "BODY:\n" );
+	printf( "%s:\n" , pckRtn2->mk_str().c_str() );
+}
+
+
+
+//
+int main(int argc, char* argv[])
+{
+	test_json();
+	test_web_recv_post();
+	test_web_send_post();
+
+	if( IsOsWin() ) system("pause");
+
+	return 0;
+}
+
+
